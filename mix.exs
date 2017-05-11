6a13c99f7f26d69407684818e4e5af98baa38bb9
cesar.guzman <cesar.guzman@gennion.com>
2017-05-11 17:19:57 +0200

    Update version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Telex.Mixfile do
 
   def project do
     [app: :telex,
-     version: "0.1.6",
+     version: "0.1.7",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
