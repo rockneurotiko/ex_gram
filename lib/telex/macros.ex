757f801631424fe7903ad1fb0a18f315eaa1053d
Miguel Garcia <miguel.garcialafuente@cabify.com>
2018-04-07 14:24:07 +0200

    Return ok tuple!

--- lib/telex/macros.ex
+++ lib/telex/macros.ex
@@ -387,7 +387,7 @@ defmodule Telex.Macros do
 
           case result do
             {:ok, body} ->
-              body |> unquote(result_transformer)
+              {:ok, body |> unquote(result_transformer)}
 
             {:error, error} ->
               {:error, error}
