080d9b493e0b75dcecdc223979dc41c1bac29f52
Miguel Garcia <miguel.garcialafuente@cabify.com>
2018-04-07 13:26:04 +0200

    Filter empty strings

--- test/support/string_generator.exs
+++ test/support/string_generator.exs
@@ -1,5 +1,5 @@
 defmodule Test.Support.StringGenerator do
-  @chars "ABCDEFGHIJKLMNOPQRSTUVWXYZ" |> String.split("")
+  @chars "ABCDEFGHIJKLMNOPQRSTUVWXYZ" |> String.split("") |> Enum.filter(&(&1 != ""))
 
   def string_of_length(length) do
     Enum.reduce(1..length, [], fn _i, acc ->
