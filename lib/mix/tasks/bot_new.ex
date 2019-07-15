0b5b3374ff93cabf4d34a4416cb22329936e41f7
Ironjanowar <ironjanowar@gmail.com>
2019-07-15 10:03:57 +0200

    Fix template path

--- lib/mix/tasks/bot_new.ex
+++ lib/mix/tasks/bot_new.ex
@@ -11,7 +11,7 @@ defmodule Mix.Tasks.Bot.New do
       app_string |> String.split("_") |> Enum.map(&String.capitalize/1) |> Enum.join("")
 
     target = "lib/#{app}/bot.ex"
-    contents = EEx.eval_file("templates/template.ex", app_module: app_module, app: app)
+    contents = EEx.eval_file("templates/bot.ex", app_module: app_module, app: app)
     create_file(target, contents)
   end
 end
