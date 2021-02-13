370b381083fa6bea4347ef6f5ad3debaf1856d2d
Ironjanowar <ironjanowar@gmail.com>
2021-02-13 23:28:25 +0100

    Add module doc for json logger adapter

--- lib/ex_gram/log_adapter/json_logger.ex
+++ lib/ex_gram/log_adapter/json_logger.ex
@@ -1,4 +1,14 @@
 defmodule ExGram.LogAdapter.JsonLogger do
+  @moduledoc """
+  Logs in a one line JSON, with the following format:
+
+    {"log_level":"<log_level>","message":"<message>","time":"<time_now>"}
+
+    - log_level -> debug, warn or error
+    - message -> string provided
+    - time -> date now with the following format: 2021-02-13 22:21:49.503110Z
+  """
+
   @behaviour ExGram.LogAdapter
 
   for log_level <- [:debug, :warn, :error] do
