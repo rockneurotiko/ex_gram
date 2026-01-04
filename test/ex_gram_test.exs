4fffeef414e84c2d7ce99530825f4f56078db70f
Pepe Márquez Romero <pepe.marquezromero@protonmail.com>
2026-01-04 14:06:31 +0100

    fix test adapter

--- test/ex_gram_test.exs
+++ test/ex_gram_test.exs
@@ -16,15 +16,22 @@ defmodule ExGramTest do
     setup do
       name = 10 |> string_of_length() |> String.to_atom()
       {:ok, _} = Test.start_link(name: name)
-      Test.start_link([])
+
+      # Start default test adapter if not already started
+      case Test.start_link([]) do
+        {:ok, _} -> :ok
+        {:error, {:already_started, _}} -> :ok
+      end
 
       {:ok, name: name}
     end
 
     test "test random", %{name: name} do
+      # Test with named adapter
       Test.backdoor_request("/getMe", %{username: "rock"}, name)
       assert {:ok, %{username: "rock"}} == Test.request(:get, "/getMe", "", name)
 
+      # Test with default adapter
       Test.backdoor_request("/getMe", %{username: "rock"})
       user = %User{username: "rock"}
       assert {:ok, user} == ExGram.get_me()
