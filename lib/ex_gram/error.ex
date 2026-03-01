64da6fd687260404fcce9b6a9a3bf3ada3b32d21
Rock <miguel.garcia@fcm.digital>
2026-03-01 18:05:43 +0100

    ExGram.Error doc

--- lib/ex_gram/error.ex
+++ lib/ex_gram/error.ex
@@ -1,5 +1,7 @@
 defmodule ExGram.Error do
-  @moduledoc false
+  @moduledoc """
+  Struct for representing errors in ExGram. It can be used to represent both errors returned by Telegram and errors that occur within the library itself.
+  """
   @type t :: %__MODULE__{code: number | atom, message: String.t() | any, metadata: any}
   defexception [:code, :message, :metadata]
 end
