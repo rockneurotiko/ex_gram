a2518fc78678943523ed608b5c3ff624b66e12b3
Rock <miguel.garcia@fcm.digital>
2026-03-10 11:22:12 +0100

    Default adapter Req

--- config/config.exs
+++ config/config.exs
@@ -1,9 +1,7 @@
 import Config
 
-config :ex_gram, token: "TOKEN", adapter: ExGram.Adapter.Tesla, json_engine: Jason, delete_webhook: true
+config :ex_gram, token: "TOKEN", adapter: ExGram.Adapter.Req, json_engine: Jason, delete_webhook: true
 
 config :logger, level: :debug
-
-config :tesla, adapter: Tesla.Adapter.Gun, disable_deprecated_builder_warning: true
 
 import_config "#{Mix.env()}.exs"
